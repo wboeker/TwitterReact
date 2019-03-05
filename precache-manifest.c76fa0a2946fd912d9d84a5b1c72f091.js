a3b4df71dde11b57622ffe5c49fc75c2d5e35721
wboeker <wendyb@alcraeft.com>
2019-03-04 21:58:11 -0800

    Updates

---
+++ precache-manifest.c76fa0a2946fd912d9d84a5b1c72f091.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a29523bbb85273b1d11",
+    "url": "/TwitterReact/static/js/runtime~main.5a29523b.js"
+  },
+  {
+    "revision": "a2c7f4c6f0f948fb26af",
+    "url": "/TwitterReact/static/js/main.a2c7f4c6.chunk.js"
+  },
+  {
+    "revision": "e0dc26a75416a1f7ef9b",
+    "url": "/TwitterReact/static/js/2.e0dc26a7.chunk.js"
+  },
+  {
+    "revision": "a2c7f4c6f0f948fb26af",
+    "url": "/TwitterReact/static/css/main.9298c381.chunk.css"
+  },
+  {
+    "revision": "f70fad7f90c8aca4cd5c5a0fbab13a5d",
+    "url": "/TwitterReact/index.html"
+  }
+];
